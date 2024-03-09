235555896063196e96beda24f2cc084ad224185d
rikamishiro <juliasjkls@gmail.com>
2024-03-09 09:31:17 -0300

    adicionando componentes e vinculando no app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListaTurmasComponent } from './components/lista-turmas/lista-turmas.component';
+import { TurmaComponent } from './components/turma/turma.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
+    ListaTurmasComponent,
+    TurmaComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
